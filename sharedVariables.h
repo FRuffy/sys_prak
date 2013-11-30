8916b4bd5f1f2dc5b8699d12e6155867dd84c068
Muahaas <fabian.ruffy@gmail.com>
2013-11-30 19:15:50 +0100

    Update sharedVariables.h

--- sharedVariables.h
+++ sharedVariables.h
@@ -19,13 +19,14 @@ typedef struct config_struct {
 int openConfig(char* name);
 int performConnection(int socket);
 
-char * playerNum; // Die gewuenschte Spielernummer die wir optional angeben koennen
 config_struct *conf; // Die Struktur, die die Konfigurationsparameter der Datei speichert
+int initConnection(int argc, char** argv);
+FILE* logdatei;
 
 struct sharedmem {
 	pid_t pidDad;
 	pid_t pidKid;
-	char gameID[14]; //Name des Spiels
+	char gameID[30]; //Name des Spiels
 	int playerNumber; //Eigene Spielernummer
 	int playerCount; //Anzahl Spieler
 } ;
