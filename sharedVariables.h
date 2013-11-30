b6191e9ab1b0fac556c7926e05de9e3db4c0f134
flo-lo <flo.lochner@campus.lmu.de>
2013-11-30 12:01:03 +0100

    Update sharedVariables.h
    jede Menge extern-Anweisungen entfernt - waren nicht nötig

--- sharedVariables.h
+++ sharedVariables.h
@@ -15,11 +15,22 @@ typedef struct config_struct {
 
 } config_struct;
 
-extern config_struct *conf;
-extern int openConfig(char* name);
-extern int performConnection(int socket);
-extern char *ID;
-extern char* playerNum;
-extern struct sharedmem *shm;
+
+int openConfig(char* name);
+int performConnection(int socket);
+
+char * playerNum; // Die gewuenschte Spielernummer die wir optional angeben koennen
+config_struct *conf; // Die Struktur, die die Konfigurationsparameter der Datei speichert
+
+struct sharedmem {
+	pid_t pidDad;
+	pid_t pidKid;
+	char gameID[14]; //Name des Spiels
+	int playerNumber; //Eigene Spielernummer
+	int playerCount; //Anzahl Spieler
+} ;
+
+struct sharedmem *shm;
+
 
 #endif
