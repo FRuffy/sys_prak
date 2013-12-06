2f6f20c0ab4c47b67f0ea282bf1fff54cd50250d
flo-lo <flo.lochner@campus.lmu.de>
2013-12-06 21:21:28 +0100

    Update Prolog.c
    Vorbereitungen zum Einhaengen des SHM "pf" (Playing Field) getroffen und dieses am Ende wieder ausgehaengt

--- Prolog.c
+++ Prolog.c
@@ -21,12 +21,12 @@ int main (int argc, char** argv )
     logdatei =fopen("log.txt","w+");
     conf = calloc(5,sizeof(config_struct));
 
-    /* Initialisierung der Shared Memory */
+    /* Initialisierung des Shared Memory */
     int shmID;
-    int shmSize = sizeof(struct sharedmem);
+    //int shmIDGameField;
     // sharedMem Hilfe: http://www.nt.fh-koeln.de/vogt/bs/animationen/SharedMemAnimation.pdf
 
-    shmID = shmget(IPC_PRIVATE, shmSize, IPC_CREAT | IPC_EXCL | 0775);
+    shmID = shmget(IPC_PRIVATE, sizeof(struct sharedmem), IPC_CREAT | IPC_EXCL | 0775);
 
     if (shmID < 1)
     {
@@ -36,13 +36,14 @@ int main (int argc, char** argv )
         return EXIT_FAILURE;
     }
 
-    shm = shmat(shmID, 0, 0); //SHM einhaengen ;
+    shm = shmat(shmID, 0, 0); //SHM einhaengen
 
     if (shm == (void *) -1)   //Im Fehlerfall pointed shm auf -1
     {
         fprintf(stderr, "Fehler, shm: %s\n", strerror(errno));
         writelog(logdatei,AT);
     }
+
     pid_t pid = 0;
     pid = fork();
 
@@ -71,10 +72,15 @@ int main (int argc, char** argv )
         //Hier kommt spaeter der Thinker hin
 
         waitpid(pid, NULL, 0); //Wartet auf sein Kind und beendet sich nach diesem
+        pf = shmat(pfID, 0, 0); //pf einhaengen // gehoert spaeter vor das waitpid, ABER: Vor dem Einhaengen sicherstellen, dass das Kind den shm pf (Playing Field)  schon reserviert hat!!! (Inter-Prozesskommunikation?)
 
+
+        shmdt(pf);
+        shmdt(shm);
     }
 
     shmctl(shmID,IPC_RMID, NULL);
+    shmctl(pfID,IPC_RMID, NULL);
     fclose(logdatei);
     free(conf);
     return EXIT_SUCCESS;
