507093de9e08ac671dfdb440b4b1de0e196afde1
Muahaas <fabian.ruffy@gmail.com>
2013-12-09 22:31:05 +0100

    Update errmmry.h

--- errmmry.h
+++ errmmry.h
@@ -1,9 +1,13 @@
 #ifndef errmmry_h
 #define errmmry_h
 
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+#define AT __FILE__ ":" TOSTRING(__LINE__)
+
 extern int varcount;
 int *varadresses[20];
-extern char *errorstring; 
+extern char *errorstring;
 int addfree(int* adr);
 int freeall();
 int writelog(FILE *filename,const char *location);
