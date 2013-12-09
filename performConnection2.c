40bacd3cff5bbd88e99e45010ef885d6d0e7c1b3
Muahaas <fabian.ruffy@gmail.com>
2013-12-09 22:27:38 +0100

    Update performConnection2.c

--- performConnection2.c
+++ performConnection2.c
@@ -2,156 +2,161 @@
 #include <string.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <sys/shm.h>
 #include <unistd.h>
 #include <errno.h>
 #include "sharedVariables.h"
 #include "errmmry.h"
+#include "auxiliaryFunctions.h"
 
-#define BUFFR 256
-#define STRINGIFY(x) #x
-#define TOSTRING(x) STRINGIFY(x)
-#define AT __FILE__ ":" TOSTRING(__LINE__)
 
-/**
-* readGameField bekommt folgendes uebergeben und liesst es in den pf-SHM ein
-* + 4 * * 15 *
-* + 3 * * * *
-* + 2 * * * *
-* + 1 * * * *
-* + ENDFIELD
-*/
-int readGameField(char *buffer2)
+#define BUFFR 512
+
+
+
+/*Formatierte Ausgabe an den Server, \n wird benoetigt um das Ende der Uebertragung zu signalisieren */
+
+int performConnection(int sock)
 {
-    int i=0, znr=0;
+    int size;//size zur Fehlerbehandlung fuer recv
+    char* reader;
+    char* temp;
+    reader = malloc(sizeof(char)*20);
+    char* buffer = malloc(sizeof(char)*BUFFR);
+    temp = malloc(sizeof(char)*30);
+    //strcpy(gameID,"ID fm1y4PiVKfU");
+    //strcpy(gameID,"ID 528902d1b0074");
 
-    while (strcmp(buffer2,"+ ENDFIELD") != 0)
+    /* Teil 1: Lese die Client-Version des Servers und antworte mit eigener (formatierten) Version
+    Behandle die Antwort des Servers */
+
+    size = recv(sock, buffer, BUFFR-1, 0);
+    sscanf(buffer, "%*s%*s%*s%s", reader);
+    if (size > 0) buffer[size]='\0';
+    printf("\nDie Version des Servers ist: %s\n", reader);
+
+    temp = antistrcat(conf->version,"VERSION ");
+    sendReplyFormatted(sock,temp);
+    free(temp);
+
+    size = recv(sock, buffer, BUFFR-1, 0);
+    if (size > 0) buffer[size]='\0';
+
+    if (buffer[0] == '-')
     {
-//Kommentarzeichen entfernen um zu kapieren wie die Funktion funktioniert!
-//         printf("\n%s\n", buffer2);
-        sscanf (buffer2,"%*s %d %[0-9* ]", &znr, buffer2);
-
-        for (i=0; i<=shm->fieldX; i++)
-        {
-            *(pf+i+(znr-1)*(shm->fieldX)) = -1;
-            sscanf (buffer2,"%d %[0-9* ]",(pf+i+(znr-1)*(shm->fieldY)), buffer2);
-//                         printf("=>%d \n", *(pf+i+(znr-1)*shm->fieldY));
-            sscanf (buffer2,"%*s %[0-9* ]",buffer2);
-//                         printf(": %s \n", buffer2);
-        }
-        buffer2 = strtok( NULL, "\n" );
+        printf("\nDer Server akzeptiert die Version %s dieses Clients nicht!\n",conf->version);
+        free(buffer);
+        free(reader);
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        printf("\nDie Client-Version wurde akzeptiert, uebertrage Spiel-ID...\n");
     }
 
-    return EXIT_SUCCESS;
-}
+    /* Teil 2: Der Server erwartet Game-ID, schicke diese und behandle Antwort.
+    Falls die ID fehlerhart oder das Spiel des Servers nicht Quarto ist wird die Verbindung beendet*/
 
-/**
-* printGameField gibt aktuellen Zustand des Spielfeldes aus
-*/
-int printGameField()
-{
-    int i,j;
-    for (i=shm->fieldY-1; i>=0; i--)
+    temp = antistrcat(shm->gameID,"ID ");
+    sendReplyFormatted(sock, temp);
+    free(temp);
+    size = recv(sock, buffer, BUFFR-1, 0);
+    if (size > 0) buffer[size]='\0';
+    sscanf(buffer, "%*s%*s%s", reader);
+
+    if (buffer[0] == '-')
     {
-        printf("\n %d: ", i+1);
-        for (j=0; j<shm->fieldX; j++)
-        {
-            if (*(pf+j+i*shm->fieldY)==-1)
-            {
-                printf(" *");
-            }
-            else
-            {
-                printf(" %d", *(pf+j+i*shm->fieldY));
-            }
-        }
+        printf("\nDie uebergebene Game-ID fehlt oder ist fehlerhaft! Beende Verbindung\n");
+        free(buffer);
+        free(reader);
+        return EXIT_FAILURE;
     }
-    printf("\n     A B C D\n");
-    return EXIT_SUCCESS;
-}
-
-
-int recvServerInfo(char* buffer)
-{
-
-    char* buffer2;
-    char* bufrptr;
-    buffer2 = malloc(sizeof(char)*512);
-    bufrptr = buffer2;
-    int i = 1;
-
-    /* sscanf(buffer,"%*s %*s %d %*s %d %s %i %*[^\n] %*s %*s %d %*s %*s %d %*s %*s %d%*[,]%d"
-            ,&(shm->playerCount),&shm->player[i].playerNumber, &shm->player[i].playerName, &shm->player[i].playerReady, &(shm->thinkTime), &(shm->nextStone)
-            ,&(shm->fieldX), &(shm->fieldY)); */
-
-    strcpy(buffer2,buffer);
-    bufrptr = buffer2;
-
-    printf("\n%s\n",buffer2);
-    buffer2 = strtok( buffer2, "\n" );
-    printf("\n%s\n",buffer2);
-
-    //Verarbeite: "+ TOTAL 2" (Anzahl der Spieler)
-    sscanf(buffer2, "%*s %*s %i", &(shm->playerCount));
-    if (shm->playerCount > 8)
+    else if (strcmp(reader,"Quarto") !=0)
     {
-        printf("\nDiese Version des Clients unterstuetzt maximal 8 Spieler!\n");
-        free(bufrptr);
+        printf("\nDas Spiel, das der Server spielt, ist nicht Quarto! Beende Verbindung.\n");
+        free(buffer);
+        free(reader);
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        printf("\nDer Server moechte %s spielen. Und wir auch!\n", reader);
+    }
+    /* Teil 3.1: Hatten wir mit unserer ID Erfolg erfahren wir zunaechst den Namen des Spiels
+    und senden die ggf. uebergebene Spielernummer.
+    Anschliessend wird die Antwort des Servers auf die Nummer behandelt und Name und Nummer des Spielerplatzes ausgelesen
+    */
+
+    size = recv(sock, buffer, BUFFR-1, 0);
+    if (size > 0) buffer[size]='\0';
+
+    sscanf(buffer, "%*s%s", shm->gameName);
+    printf("\nSpiel: %s\n", shm->gameName); //Zeige Spielnamen an, schneide das "+" ab
+    temp = antistrcat(conf -> playernumber,"PLAYER ");
+    sendReplyFormatted(sock,temp);
+    free(temp);
+    size = recv(sock, buffer, BUFFR-1, 0);
+    if (size > 0) buffer[size]='\0';
+
+
+    if (buffer[0] == '-')
+    {
+        if (buffer[2] == '-')
+        {
+            printf("\nEs wurde eine ungueltige Spielernummer eingegeben! Beende Verbindung\n");
+        }
+        else
+        {
+            printf("\nAlle Plaetze sind bereits belegt, versuchen sie es spaeter noch einmal!\n");
+            //ACHTUNG. Diese Meldung erscheint auch, wenn man in der client.conf eine Spielernummer an gibt, die beim Erstellen des Spiels einem Computerspieler zugeordnet wurde!
+        }
+        free(buffer);
+        free(reader);
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        sscanf(buffer, "%*s %*s %d %s",&(shm->player[0].playerNumber), (shm->player[0].playerName));
+        //printf("\nDu spielst mit dem Namen %s, deine Nummer ist %d\n", shm->player[0].playerName,shm->player[0].playerNumber);
+    }
+    /* Teil 3.2: Hier wird der Uebergang in die Spielverlaufsphase eingeleitet.
+    Der Server sendet uns die Namen unseres Gegenspielers und des Spielernummer, wobei ueberprueft wird ob ein Spieler bereits verbunden ist.
+    */
+    size = recv(sock, buffer, BUFFR-1, 0);
+    if (size > 0) buffer[size]='\0';
+    if (buffer[0] == '-')
+    {
+        printf("\nFehler bei Uebermittlung der Spielparameter!\n");
+        free(buffer);
+        free(reader);
         return EXIT_FAILURE;
     }
 
-    //Verarbeite: "+ 1 GEGENSPIELERNAME 0" (Uebermittlung der Gegenspieler
-    //(Nummer, Name, Flag ob bereit) solange bis Server "+ ENDPLAYERS" sendet)
-    buffer2 = strtok( NULL, "\n" );
-    while (strcmp(buffer2,"+ ENDPLAYERS") != 0)
+ /*   if (recvServerInfo(buffer) == NULL)
     {
-        sscanf(buffer2, "%*s %d %s %i", &shm->player[i].playerNumber, shm->player[i].playerName, &shm->player[i].playerReady);
-        buffer2 = strtok( NULL, "\n" );
-        i++;
-    }
-    printf("\nEs spielen diese %i Spieler:\n", shm->playerCount);
-    printf("\nSpielernummer\tSpielername\tBereit\n");
-    for (i=0; i<shm->playerCount; i++)
-    {
-        printf("%d\t\t%s\t\t%d\n", shm->player[i].playerNumber, shm->player[i].playerName, shm->player[i].playerReady);
-    }
-    buffer2 = strtok( NULL, "\n" );
-
-    //Verarbeite: "+ MOVE 3000" (Zeit zur uebermittlung des naechsten Zuges)
-    sscanf(buffer2, "%*s %*s %d", &(shm->thinkTime));
-    printf("\nFuer deinen Zug hast du %d ms, ",shm->thinkTime);
-    buffer2 = strtok( NULL, "\n" );
-
-    //Verarbeite: "+ NEXT 4" (Naechster zu setzender Spielstein)
-    sscanf(buffer2, "%*s %*s %d", &(shm->nextStone));
-    printf("du musst Stein %d setzen!\n\n",shm->nextStone);
-    buffer2 = strtok( NULL, "\n" );
-
-    //Verarbeite: "+ FIELD 4,4" (Spielfeldgroesse)
-    sscanf(buffer2, "%*s %*s %d%*[,]%d", &(shm->fieldX), &(shm->fieldY));
-    printf("Unser momentanes Spielfeld. Groesse: %d x %d\n",shm->fieldX, shm->fieldY);
-
-    //Wir kennen jetzt die Spielfeldgroesse => SHM-pf (Playing Field) dafuer reservieren und einhaengen (2x Groesse von fieldX wegen 4 Merkmalen pro Stein!)
-    pfID = shmget(IPC_PRIVATE, (sizeof(short)*(shm->fieldX)*(shm->fieldX)*(shm->fieldY)), IPC_CREAT | IPC_EXCL | 0775);
-    if (pfID < 1)
-    {
-        printf("Error: No pf-SHM");
-        free(bufrptr);
-
+        free(buffer);
+        free(reader);
         return EXIT_FAILURE;
     }
-    pf = shmat(pfID, 0, 0); //pf einhaengen
-    if (pf == (void *) -1) //Im Fehlerfall pointed pf auf -1
-    {
-        fprintf(stderr, "Fehler, pf-shm: %s\n", strerror(errno));
-        writelog(logdatei,AT);
-    }
-    buffer2 = strtok( NULL, "\n" );
 
-    readGameField(buffer2);
-    printGameField();
-    free(bufrptr);
+*/
+    strcpy(buffer, recvServerInfo(buffer));
+    checkServerReply(sock,buffer);
+
+  sendReplyFormatted(sock, "THINKING");
+   size = recv(sock, buffer, BUFFR-1, 0);
+  if (size > 0) buffer[size]='\0';
+    printf("\n%s\n",buffer);
+
+
+  sendReplyFormatted(sock, "PLAY A2,10");
+
+   size = recv(sock, buffer, BUFFR-1, 0);
+if (size > 0) buffer[size]='\0';
+
+    printf("\n%s\n",buffer);
+
+
+    free(buffer);
+    free(reader);
     return EXIT_SUCCESS;
 }
