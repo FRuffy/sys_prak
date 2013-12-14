da1ff63729233f08cc15bd049b1da3e95735abad
nur4h <nurah.spahic@gmail.com>
2013-12-14 18:05:17 +0100

    Update errmmry.h

--- errmmry.h
+++ errmmry.h
@@ -5,10 +5,13 @@
 #define TOSTRING(x) STRINGIFY(x)
 #define AT __FILE__ ":" TOSTRING(__LINE__)
 
-extern int varcount;
-int *varadresses[20];
-extern char *errorstring;
-int addfree(int* adr);
+int countint;
+int countchar;
+int *varadr_int[50];
+char *varadr_char[50];
+char *errorstring;
+int addint(int* intadr);
+int addchar(char* charadr);
 int freeall();
 int writelog(FILE *filename,const char *location);
 #endif
