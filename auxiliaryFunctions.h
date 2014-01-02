6fba4b1bb2b47bc17d64ffd4b8059c2b04b25a5e
Muahaas <fabian.ruffy@gmail.com>
2014-01-02 14:16:51 +0100

    Update auxiliaryFunctions.h

--- auxiliaryFunctions.h
+++ auxiliaryFunctions.h
@@ -3,6 +3,7 @@
 
 
 int openConfig(char* name, config_struct* conf);
+int openConfig(char* name, config_struct* conf);
 int performConnection(int sock,sharedmem * shm, config_struct* conf);
 char*  recvServerInfo(char* buffer,sharedmem * shm);
 int checkServerReply(int sock, char* buffer,sharedmem * shm);
@@ -11,7 +12,7 @@ void sendReplyFormatted(int sock, char* reply);
 char* antistrcat(char* dest, char* src, char* temp);
 int readGameField(char *buffer,sharedmem * shm);
 int printGameField( sharedmem *shm);
-int printGameFieldQuarto( sharedmem *shm);
+int printGameFieldQuarto4x4(sharedmem * shm);
 void loop(int sock, char* buffer, sharedmem * shm);
 void think( sharedmem *shm);
 char* byte_to_binary(int n);
