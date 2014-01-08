b884d3869175a90571a72477f48f77d6b2e1b391
flo-lo <flo.lochner@campus.lmu.de>
2014-01-08 18:55:03 +0100

    Update thinker.h
    thinker.h gefüllt

--- thinker.h
+++ thinker.h
@@ -1,5 +1,11 @@
 #ifndef thinker_h
 #define thinker_h
-// @Fabian bitte deinen teil des thinker.c hier einfuegen.
+
+void think( sharedmem *shm);
+char* formatMove(int move);
+char* formatMove1(int move);
+int testStone(sharedmem * shm, int stone);
+void chooseStone(sharedmem * shm);
+void think(sharedmem * shm);
 
 #endif
