499c24f5aed2e519577555d63a8c3dd1ce7087b6
flo-lo <flo.lochner@campus.lmu.de>
2014-01-08 18:53:21 +0100

    Update auxiliaryFunctions.h
    thinker nach think.h verschoben, loop entfernt (wird nicht mehr benötigt)

--- auxiliaryFunctions.h
+++ auxiliaryFunctions.h
@@ -13,8 +13,6 @@ char* antistrcat(char* dest, char* src, char* temp);
 int readGameField(char *buffer,sharedmem * shm);
 int printGameField( sharedmem *shm);
 int printGameFieldQuarto4x4(sharedmem * shm);
-void loop(int sock, char* buffer, sharedmem * shm);
-void think( sharedmem *shm);
 char* byte_to_binary(int n);
 int calculateMove(sharedmem *shm);
 #endif
