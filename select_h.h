8eda7c6210c079371ce1493b37c44bf51e92cf33
flo-lo <flo.lochner@campus.lmu.de>
2014-01-08 20:20:40 +0100

    Delete select_h.h
    Inhalt in "select.h" integriert.

--- select_h.h
+++
@@ -1,6 +0,0 @@
-#ifndef select_h
-#define select_h
-
-int waitforfds(int socket,char* buffer,sharedmem * shm); // nur ein Vorschlag der Parametrisierung, bitte feststellen,
-                                                   	//was tatsaechlich benoetigt wird
-#endif
