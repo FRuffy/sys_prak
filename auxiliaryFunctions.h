14248e150059a999f142e73956531d64db91b2bf
flo-lo <flo.lochner@campus.lmu.de>
2014-01-08 20:16:19 +0100

    Update auxiliaryFunctions.h
    calculateMove in jeweilige Header-Datei verschoben

--- auxiliaryFunctions.h
+++ auxiliaryFunctions.h
@@ -7,5 +7,5 @@ int readGameField(char *buffer,sharedmem * shm);
 int printGameField( sharedmem *shm);
 int printGameFieldQuarto4x4(sharedmem * shm);
 char* byte_to_binary(int n);
-int calculateMove(sharedmem *shm);
+
 #endif
