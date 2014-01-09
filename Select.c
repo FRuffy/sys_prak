ee4e0fed060859cd2b164e1b605755bf65c2dfac
Edd <edina90@hotmail.com>
2014-01-09 03:16:51 +0100

    Update Select.c

--- Select.c
+++ Select.c
@@ -8,9 +8,9 @@
 #include <errno.h>
 #include <signal.h>
 #include <sys/shm.h>
-#include "sharedVariables.h"
-#include "errmmry.h"
-#include "auxiliaryFunctions.h"
+#include "SharedVariables.h"
+#include "Errmmry.h"
+#include "AuxiliaryFunctions.h"
 #include "Playtime.h"
 
 #define BUFFR 512
@@ -27,22 +27,30 @@
  * @param  variable Description text text text.          
  * @return Description text text text.
  */
-int doMove(int sock, char* buffer)
-{
+int doMove(int sock, char* buffer) {
   int size;
   char* reply = malloc(sizeof(char)*15);
   size= read(fd[0], reply, 15);
-  if (size < 0) perror("Fehler bei read");
+  
+  if (size < 0) {
+  	perror("Fehler bei read");
+  }
   printf("\nReplyDoMove: %s\n", reply);
   sendReplyFormatted(sock, reply);
   free(reply);
 	
   size = recv(sock, buffer, BUFFR - 1, 0);
-  if (size > 0) buffer[size] = '\0';
-  if (strcmp(buffer, "+ MOVEOK\n") == 0)  printf("Zug wurde akzeptiert!\n");
-	else return EXIT_FAILURE;
-		
-	return EXIT_SUCCESS;
+  if (size > 0) {
+  	buffer[size] = '\0';
+  }
+  
+  if (strcmp(buffer, "+ MOVEOK\n") == 0) {
+  	printf("Zug wurde akzeptiert!\n");
+  } else {
+  	return EXIT_FAILURE;
+  }
+  
+  return EXIT_SUCCESS;
 }
 
 /**
@@ -52,27 +60,25 @@ int doMove(int sock, char* buffer)
 * @param Socket fuer Kommunikation mit Server, buffer fuer auslesen der Antwort, Shared Memory
 * @return on success, 0 on failure 1
 **/
-int waitforfds(int sock,char* buffer,sharedmem * shm) // nur ein Vorschlag der Parametrisierung, bitte feststellen,
-
-{
+int waitforfds(int sock,char* buffer,sharedmem * shm) { // nur ein Vorschlag der Parametrisierung, bitte feststellen,
   // Vorbereitungen fuer select() siehe man select
-  int rc,size,status,biggest;
+  int rc, size, status, biggest;
   int pipe=fd[0];
   fd_set fds;
   struct timeval timeout;
 
-  if (pipe>sock) biggest=pipe;
-  else biggest=sock;
-
+  if (pipe>sock) {
+  	biggest=pipe;
+  } else {
+  	biggest=sock;
+  }
 	
   /*
   * checkServerReply muss leider einmal ausserhalb der Schleife ausgefuehrt werden, da die Antwort des Servers schon frueher im Code 
   * vom Socket gelesen wurde und hier im buffer uebergeben wurde
   */
   checkServerReply(sock, buffer, shm);
-
-  do 
-  {	
+  do {	
  	/* Unsere 2 Filedescriptoren werden hinzugefuegt. (Socket und Pipe). */
    	FD_ZERO(&fds);
    	FD_SET(sock, &fds);
@@ -89,34 +95,37 @@ int waitforfds(int sock,char* buffer,sharedmem * shm) // nur ein Vorschlag der P
     * Filedescriptoren zurueck. Sollte  
     */
     rc = select(biggest+1, &fds, NULL, NULL, &timeout);
-    if (rc==-1) 
-    {
+    if (rc==-1) {
       perror("Error, select failed! \n");
       return EXIT_FAILURE;
     }
 
-    if (rc > 0)
-    {	
+    if (rc > 0) {	
       printf("SELECT: Number of FDs ready: %d \n",rc);
       // Fall das Socket ready to read ist:
-      if (FD_ISSET(sock, &fds)) 
-      {
-		    printf("SELECT: Socket is ready to read!\n");
-        size = recv(sock, buffer, BUFFR - 1, 0); writelog(logdatei,AT);
+      if (FD_ISSET(sock, &fds)) {
+	printf("SELECT: Socket is ready to read!\n");
+        size = recv(sock, buffer, BUFFR - 1, 0); 
+        writelog(logdatei,AT);
         printf("\nBuffer fuer Playtime: %s\n", buffer);
-				if (size > 0)                        buffer[size] = '\0';
+	if (size > 0) {
+		buffer[size] = '\0';
+	}
         status = checkServerReply(sock, buffer, shm);
-        if (status != 0) break;
-      }
-            // Fall das Pipe ready to read ist:
-      else if (FD_ISSET(pipe, &fds)) 
-      {
-		    printf("SELECT: Pipe is ready to read!\n");
+        if (status != 0) {
+        	break;
+        }
+      } else if (FD_ISSET(pipe, &fds)) {  // Fall das Pipe ready to read ist:
+     	printf("SELECT: Pipe is ready to read!\n");
         doMove(sock, buffer);
       }
-         // falls select 0 returned heisst die Wartezeit ist ohne Ereigniss abgelaufen
-    } else if (rc==0) perror("Select timed out!. \n");
-  } while (rc!=0);
-    // solange es zu keinem Timeout kommt, soll die Kommunikation in der Schleife weiterlaufen.
-   return EXIT_SUCCESS; // Falls die Schleife ohne Probleme zu Ende durchlaeuft
+      // falls select 0 returned heisst die Wartezeit ist ohne Ereigniss abgelaufen
+    } else if (rc==0) { 
+    	perror("Select timed out!. \n"); 
+    }
+  } 
+  
+  while (rc!=0);
+  // solange es zu keinem Timeout kommt, soll die Kommunikation in der Schleife weiterlaufen.
+  return EXIT_SUCCESS; // Falls die Schleife ohne Probleme zu Ende durchlaeuft
 }
