2ef879ab032d91fbcafd3f7e1eb80d77612c28a6
Edd <edina90@hotmail.com>
2014-01-09 02:12:08 +0100

    Update InitConnection.h

--- InitConnection.h
+++ InitConnection.h
@@ -1,5 +1,5 @@
-#ifndef initConnection_h
-#define initConnection_h
+#ifndef InitConnection_h
+#define InitConnection_h
 
 int initConnection(int argc, char** argv,sharedmem * shm, config_struct* conf);
 
