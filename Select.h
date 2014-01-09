0f9c263ae9ad2a29edc35deb65472466dcc0af9d
Edd <edina90@hotmail.com>
2014-01-09 03:04:45 +0100

    Update Select.h

--- Select.h
+++ Select.h
@@ -1,5 +1,5 @@
-#ifndef select_h
-#define select_h
+#ifndef Select_h
+#define Select_h
 
 int waitforfds(int socket,char* buffer,sharedmem * shm); // nur ein Vorschlag der Parametrisierung, bitte feststellen,
                                                         //was tatsaechlich benoetigt wird
