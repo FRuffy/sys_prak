ed18665e5b774332edead542fe97ed88d01a79c1
flo-lo <flo.lochner@campus.lmu.de>
2014-01-12 14:19:21 +0100

    Update and rename Thinker.h to QuartoThinker.h
    "printGameFieldQuarto4x4" und "byte_to_binary" von AuxiliaryFunctions.c nach QuartoThiner.c verschoben.

--- QuartoThinker.h
+++ QuartoThinker.h
@@ -1,5 +1,5 @@
-#ifndef Thinker_h
-#define Thinker_h
+#ifndef QuartoThinker_h
+#define QuartoThinker_h
 
 int think( sharedmem *shm);
 char* formatMove(int move);
@@ -7,5 +7,7 @@ char* formatMove1(int move);
 int testStone(sharedmem * shm, int stone);
 void chooseStone(sharedmem * shm);
 int calculateMove(sharedmem *shm, char* stones);
+int printGameFieldQuarto4x4(sharedmem * shm, char* stones);
+int byte_to_binary(int n, char* container);
 
 #endif
