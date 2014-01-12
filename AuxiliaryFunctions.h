06953100a56c3abe6c91b25fcf9a20027d38f4e3
flo-lo <flo.lochner@campus.lmu.de>
2014-01-12 14:20:28 +0100

    Update AuxiliaryFunctions.h
    "printGameFieldQuarto4x4" und "byte_to_binary" von AuxiliaryFunctions.c nach QuartoThiner.c verschoben.

--- AuxiliaryFunctions.h
+++ AuxiliaryFunctions.h
@@ -5,7 +5,5 @@ void sendReplyFormatted(int sock, char* reply);
 char* antistrcat(char* dest, char* src, char* temp);
 int readGameField(char *buffer,sharedmem * shm);
 int printGameField( sharedmem *shm);
-int printGameFieldQuarto4x4(sharedmem * shm, char* stones);
-int byte_to_binary(int n, char* container);
 
 #endif
