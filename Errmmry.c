7e926f3a3e8d5197be2e5c1f49de558147d9d90b
Edd <edina90@hotmail.com>
2014-01-15 20:46:41 +0100

    Update Errmmry.c

--- Errmmry.c
+++ Errmmry.c
@@ -5,86 +5,75 @@
 #include "Errmmry.h"
 #include <time.h>
 
-int countint=0; // Anzahl der zugewiesenen Speicherbereiche
-int countchar=0;
-
-
+// Anzahl der zugewiesenen Speicherbereiche
+int countint = 0;
+int countchar = 0;
 
 /**
- * Fuegt den Pointer auf den spaeter zu befreienden Bereich in ein Array hinzu.
+ * Fuegt den Pointer auf den spaeter zu befreienden Bereich in ein Array hinzu
  *
- * @param  adr Der Pointer auf den Speicherbereich
- * @return Gibt 0 zurueck.
+ * @param adr der Pointer auf den Speicherbereich
+ * @return 0 / 1
  */
-
-
-
 int addint(int* intadr) {
+	if (intadr == NULL ) {
+		perror("Fehler bei Malloc eines int pointers! \n");
+		exit(EXIT_FAILURE);
+	}
 
-        if (intadr==NULL){
-                perror("Fehler bei Malloc eines int pointers! \n");
-                exit(EXIT_FAILURE);
-        }
-
-        varadr_int[countint]=intadr;
-        countint++;
-        return EXIT_SUCCESS;
+	varadr_int[countint] = intadr;
+	countint++;
+	return EXIT_SUCCESS;
 }
-
 
 int addchar(char* charadr) {
+	if (charadr == NULL ) {
+		perror("Fehler bei Malloc eines char pointers! \n");
+		exit(EXIT_FAILURE);
+	}
 
-        if (charadr==NULL){
-                perror("Fehler bei Malloc eines char pointers! \n");
-               exit(EXIT_FAILURE);
-        }
-
-        varadr_char[countchar]=charadr;
-       countchar++;
-        return EXIT_SUCCESS;
+	varadr_char[countchar] = charadr;
+	countchar++;
+	return EXIT_SUCCESS;
 }
-
 
 /**
- * Befreit alle in das Array bisher aufgenommenen Speicherbereiche.
+ * Befreit alle in das Array bisher aufgenommenen Speicherbereiche
  *
- * @return Gibt 0 zurueck.
+ * @return 0
  */
 int freeall() {
-        int i;
+	int i;
 
-        for(i=0;i<=countint;i++) {
-        free(varadr_int[i]);
-        }
+	for (i = 0; i <= countint; i++) {
+		free(varadr_int[i]);
+	}
 
-        for(i=0;i<=countchar;i++) {
-        free(varadr_char[i]);
-        }
+	for (i = 0; i <= countchar; i++) {
+		free(varadr_char[i]);
+	}
 
-       // printf("Es wurden %d Variablen befreit \n",countint+countchar);
-        return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
-
 
 /**
  * Schreibt eine Fehlermeldung mit Timestamp in die Datei log.txt
- * Um Zeilenzahl genau zu halten, bitte neben Syscalls aufrufen.
+ * Um Zeilenzahl genau zu halten, bitte neben Syscalls aufrufen
  *
- * @param Filepointer zur log Datei
- * @param String im Format: "Dateipfad:Zeilennummer".
- * @return Gibt 0 zurueck.
+ * @param Filepointer zur log Datei, String im Format: "Dateipfad:Zeilennummer"
+ * @return 0
  */
-int writelog(FILE *filename,const char *location) {
-        time_t result = time(NULL);
-        char *t = ctime(&result);
-        t[strlen(t)-1] = 0;
+int writelog(FILE *filename, const char *location) {
+	time_t result = time(NULL );
+	char *t = ctime(&result);
+	t[strlen(t) - 1] = 0;
 
-        if (errno!=0) {
-                printf("Ein Fehler wurde in der Logdatei dokumentiert. \n");
-        }
+	if (errno != 0) {
+		printf("Ein Fehler wurde in der Logdatei dokumentiert. \n");
+	}
 
-        fprintf(filename,"%s --> ",t);
-        fputs(strerror(errno),filename);
-        fprintf(filename," at %s \n",location);
-        return EXIT_SUCCESS;
+	fprintf(filename, "%s --> ", t);
+	fputs(strerror(errno), filename);
+	fprintf(filename, " at %s \n", location);
+	return EXIT_SUCCESS;
 }
