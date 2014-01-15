97dcf9ead23fb752330babd1e025b7a2a264d53e
Edd <edina90@hotmail.com>
2014-01-15 20:27:38 +0100

    Update PerformConnection.h

--- PerformConnection.h
+++ PerformConnection.h
@@ -1,6 +1,6 @@
 #ifndef PerformConnection_h
 #define PerformConnection_h
 
-int performConnection(int sock,sharedmem * shm, config_struct* conf);
+int performConnection(int sock, sharedmem * shm, config_struct* conf);
 
 #endif
