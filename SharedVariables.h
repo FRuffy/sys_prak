09633092ade062200ccc45261bdffc1ec9bc0f8f
Edd <edina90@hotmail.com>
2014-01-15 20:32:23 +0100

    Update SharedVariables.h

--- SharedVariables.h
+++ SharedVariables.h
@@ -7,26 +7,27 @@
 #define KEY 5678
 
 typedef struct config_struct {
-    char hostname[100];
-    char gamekindname[20];
-    char version[5];
-    char playernumber[5];
-    int portnumber;
+	char hostname[100];
+	char gamekindname[20];
+	char version[5];
+	char playernumber[5];
+	int portnumber;
 } config_struct;
 
 typedef struct sharedmem {
-        pid_t pidDad;
-        pid_t pidKid;
-        char gameID[MAXGAMEID], gameName[MAXGAMENAME], nextField[3];
-        int playerCount, thinkTime, thinking, pleaseThink, fieldX, fieldY, StoneToPlace, nextStone, pfID;
-        int *pf;
-        struct spieler {
-                int playerNumber;
-                char playerName[MAXPLAYERNAME];
-                int playerReady;
-                int playerRegisterd;
-        } player[8];
-}sharedmem;
+	pid_t pidDad;
+	pid_t pidKid;
+	char gameID[MAXGAMEID], gameName[MAXGAMENAME], nextField[3];
+	int playerCount, thinkTime, thinking, pleaseThink, fieldX, fieldY,
+			StoneToPlace, nextStone, pfID;
+	int *pf;
+	struct spieler {
+		int playerNumber;
+		char playerName[MAXPLAYERNAME];
+		int playerReady;
+		int playerRegisterd;
+	} player[8];
+} sharedmem;
 
 int fd[2]; // fuer Pipe
 //sharedmem *shm; // shm MUSS global sein (wegen Signal)
