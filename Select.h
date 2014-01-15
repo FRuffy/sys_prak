1d525601816ceb8c6ae117c81164a9f6aa0d8bf6
Edd <edina90@hotmail.com>
2014-01-15 20:30:12 +0100

    Update Select.h

--- Select.h
+++ Select.h
@@ -1,8 +1,7 @@
 #ifndef Select_h
 #define Select_h
 
-int waitforfds(int socket,char* buffer,sharedmem * shm); // nur ein Vorschlag der Parametrisierung, bitte feststellen,
-                                                        //was tatsaechlich benoetigt wird
+int waitforfds(int socket, char* buffer, sharedmem * shm); // nur ein Vorschlag der Parametrisierung, bitte feststellen, was tatsaechlich benoetigt wird
 int doMove(int sock, char* buffer);
 
 #endif
