da54f44a293e3d0f2e417ab179a6bce85ecf5d51
Edd <edina90@hotmail.com>
2014-01-15 22:52:25 +0100

    Update ServerInfo.c

--- ServerInfo.c
+++ ServerInfo.c
@@ -6,52 +6,49 @@
 #include "SharedVariables.h"
 #include "Errmmry.h"
 
-char* recvServerInfo(char* buffer,sharedmem * shm)
-{
-       // printf("\nrecServerInfo Buffer: %s",buffer);
+/**
+ * Informationen ueber Spieler
+ *
+ * @param Pointer auf Buffer, SHM
+ * @return Info ueber Spielernummer, Spielername, Bereit
+ */
+char* recvServerInfo(char* buffer, sharedmem * shm) {
+	char* buffer2;
+	char* bfrptr; // Wichtig, falls strtok zur Modifikation des Strings verwendet wird. Der Pointer aendert sich im Verlauf, die geschriebenen Daten aber nicht
+	buffer2 = malloc(sizeof(char) * 256);
+	bfrptr = buffer2;
+	addchar(bfrptr);
+	int i = 1;
+	strcpy(buffer2, buffer);
+	buffer2 = strtok(buffer2, "\n");
 
-    char* buffer2;
-    char* bfrptr; //Wichtig, falls strtok zur Modifikation des Strings verwendet wird.Der Pointer ändert sich im Verlauf, die geschriebenen Daten aber nicht.
-    buffer2 = malloc(sizeof(char)*256);
-    bfrptr = buffer2;   addchar(bfrptr);
-    int i = 1;
-    strcpy(buffer2,buffer);
-     buffer2 = strtok( buffer2, "\n" );
+	/* Verarbeite: "+ TOTAL 2" (Anzahl der Spieler) */
+	sscanf(buffer2, "%*s %*s %i", &(shm->playerCount));
+	if (shm->playerCount > 8) {
+		printf("\nDiese Version des Clients unterstuetzt maximal 8 Spieler, die Anzahl ist jedoch %d Spieler!\n", shm->playerCount);
+		return NULL ;
+	}
 
-    //Verarbeite: "+ TOTAL 2" (Anzahl der Spieler)
-    sscanf(buffer2, "%*s %*s %i", &(shm->playerCount));
-    if (shm->playerCount > 8)
-    {
-        printf("\nDiese Version des Clients unterstuetzt maximal 8 Spieler, die Anzahl ist jedoch %d Spieler!\n",shm->playerCount);
+	/* Verarbeite: "+ 1 GEGENSPIELERNAME 0" (Uebermittlung der Gegenspieler
+	 * (Nummer, Name, Flag ob bereit) solange bis Server "+ ENDPLAYERS" sendet) */
+	buffer2 = strtok(NULL, "\n");
+	while (strcmp(buffer2, "+ ENDPLAYERS") != 0) {
+		sscanf(buffer2, "%*s %d %s %i", &shm->player[i].playerNumber, shm->player[i].playerName, &shm->player[i].playerReady); buffer2 = strtok(NULL, "\n");
+		i++;
+	}
+	printf("\nWir sind Spieler: %s\n", shm->player[0].playerName);
+	printf("\nInsgesamt spielen diese %i Spieler:\n", shm->playerCount);
+	printf("\n%-15s %-25s %-2s\n", "Spielernummer", "Spielername", "Bereit");
+	/* Wir selbst sind natürlich immer bereit. */
+	printf("%-15d %-25s %-2d\n", shm->player[0].playerNumber, shm->player[0].playerName, 1);
 
-        return NULL;
-    }
+	// Funktioniert eigentlich aber Server hat hier anscheinend einen Bug und sendet falsche Spielernummer
+	for (i = 1; i < shm->playerCount; i++) {
+		printf("%-15d %-25s %-2d\n", shm->player[i].playerNumber, shm->player[i].playerName, shm->player[i].playerReady);
+	}
 
-    //Verarbeite: "+ 1 GEGENSPIELERNAME 0" (Uebermittlung der Gegenspieler
-    //(Nummer, Name, Flag ob bereit) solange bis Server "+ ENDPLAYERS" sendet)
-    buffer2 = strtok( NULL, "\n" );
-    while (strcmp(buffer2,"+ ENDPLAYERS") != 0)
-    {
-        sscanf(buffer2, "%*s %d %s %i", &shm->player[i].playerNumber, shm->player[i].playerName, &shm->player[i].playerReady);
-
-       buffer2 = strtok( NULL, "\n" );
-        i++;
-    }
-
-    printf("\nWir sind Spieler: %s\n",shm->player[0].playerName);
-    printf("\nInsgesamt spielen diese %i Spieler:\n", shm->playerCount);
-    printf("\n%-15s %-25s %-2s\n","Spielernummer","Spielername","Bereit");
-    //Wir selbst sind natürlich immer bereit.
- printf("%-15d %-25s %-2d\n",shm->player[0].playerNumber, shm->player[0].playerName, 1);
-    for (i=1; i<shm->playerCount; i++)
-    {
-       //Funktioniert eigentlich aber Server hat hier anscheinend einen Bug und sendet falsche Spielernummer.
-
-        printf("%-15d %-25s %-2d\n",shm->player[i].playerNumber, shm->player[i].playerName, shm->player[i].playerReady);
-
-    }
- printf("\n");
-    buffer2 = strtok( NULL, "" );
-    strcpy(buffer, buffer2);
-    return buffer;
+	printf("\n");
+	buffer2 = strtok(NULL, "");
+	strcpy(buffer, buffer2);
+	return buffer;
 }
