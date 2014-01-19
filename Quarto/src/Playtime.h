2beeff7f62fb2a30c6b85e58712e2805d8ca0559
flo-lo <flo.lochner@campus.lmu.de>
2014-01-19 15:53:03 +0100

    Update Playtime.h
    Inhalt von "AuxiliaryFunctions.c" in "PerformConnection.c" und "Playtime.c" verschoben.

--- Quarto/src/Playtime.h
+++ Quarto/src/Playtime.h
@@ -1,6 +1,8 @@
 #ifndef Playtime_h
 #define Playtime_h
 
+int readGameField(char *buffer, sharedmem * shm);
+int printGameField(sharedmem *shm);
 int checkServerReply(int sock, char* buffer, sharedmem * shm);
 
 #endif
