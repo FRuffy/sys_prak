a54581ce20f1193f54973955f0391650c8d4b4f4
flo-lo <flo.lochner@campus.lmu.de>
2014-01-19 15:52:44 +0100

    Update PerformConnection.h
    Inhalt von "AuxiliaryFunctions.c" in "PerformConnection.c" und "Playtime.c" verschoben.

--- Quarto/src/PerformConnection.h
+++ Quarto/src/PerformConnection.h
@@ -1,6 +1,8 @@
 #ifndef PerformConnection_h
 #define PerformConnection_h
 
+void sendReplyFormatted(int sock, char* reply);
+char* antistrcat(char* dest, char* src, char* temp);
 int performConnection(int sock, sharedmem * shm, config_struct* conf);
 
 #endif
