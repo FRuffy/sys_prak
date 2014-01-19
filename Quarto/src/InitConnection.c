499f8fa82a1c17a874ee9a7e043c3a138bffc88f
flo-lo <flo.lochner@campus.lmu.de>
2014-01-19 15:21:38 +0100

    Update InitConnection.c
    sock in den SHM gelegt (für korrektes closen des Sockets bei CTRL + c)

--- Quarto/src/InitConnection.c
+++ Quarto/src/InitConnection.c
@@ -11,46 +11,46 @@
 #include "PerformConnection.h"
 
 /**
- * Herstellung der Verbindung zum Server
- *
- * @param Pointer auf SHM, Struktur
- * @return 0 falls Verbindung hergestellt und performConnection ausfuehrbar
- */
+* Herstellung der Verbindung zum Server
+*
+* @param Pointer auf SHM, Struktur
+* @return 0 falls Verbindung hergestellt und performConnection ausfuehrbar
+*/
 int initConnection(sharedmem * shm, config_struct* conf) {
-	int sock = socket(AF_INET, SOCK_STREAM, 0);
-	writelog(logdatei, AT);
-	struct sockaddr_in host;
-	struct hostent* ip;
+        shm->sock = socket(AF_INET, SOCK_STREAM, 0);
+        writelog(logdatei, AT);
+        struct sockaddr_in host;
+        struct hostent* ip;
 
-	/* Uebersetzt Hostname in IP Adresse */
-	ip = (gethostbyname(conf->hostname));
-        if  (ip== NULL) {
+        /* Uebersetzt Hostname in IP Adresse */
+        ip = (gethostbyname(conf->hostname));
+        if (ip== NULL) {
              perror("\nFehler beim Anfordern der IP");
              writelog(logdatei, AT);
         }
-             
-	memcpy(&(host.sin_addr), ip->h_addr,ip ->h_length);
-	host.sin_family = AF_INET;
-	host.sin_port = htons(conf->portnumber);
 
-	if (connect(sock, (struct sockaddr*) &host, sizeof(host)) == 0) {
-		printf("\nVerbindung mit %s hergestellt!\n", conf->hostname);
-		writelog(logdatei, AT);
-	} else {
-		perror("\n Fehler beim Verbindungsaufbau");
-		return EXIT_FAILURE;
-	}
+        memcpy(&(host.sin_addr), ip->h_addr,ip ->h_length);
+        host.sin_family = AF_INET;
+        host.sin_port = htons(conf->portnumber);
 
-	/* PID in SHM schrieben */
-	shm->pidDad = getppid();
-	shm->pidKid = getpid();
+        if (connect(shm->sock, (struct sockaddr*) &host, sizeof(host)) == 0) {
+                printf("\nVerbindung mit %s hergestellt!\n", conf->hostname);
+                writelog(logdatei, AT);
+        } else {
+                perror("\n Fehler beim Verbindungsaufbau");
+                return EXIT_FAILURE;
+        }
 
-	/*Fuehre Prolog Protokoll aus */
-	if (performConnection(sock, shm, conf) != 0) {
-		close(sock);
-		return EXIT_FAILURE;
-	}
+        /* PID in SHM schrieben */
+        shm->pidDad = getppid();
+        shm->pidKid = getpid();
 
-	close(sock);
-	return EXIT_SUCCESS;
+        /*Fuehre Prolog Protokoll aus */
+        if (performConnection(shm->sock, shm, conf) != 0) {
+                close(shm->sock);
+                return EXIT_FAILURE;
+        }
+
+        close(shm->sock);
+        return EXIT_SUCCESS;
 }
