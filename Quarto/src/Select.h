9a02a771384d5737491a0881f368699f84fa19a4
nur4h <nurah.spahic@gmail.com>
2014-01-19 19:54:15 +0100

    Update Select.h
    Parametrisierung von Pipe

--- Quarto/src/Select.h
+++ Quarto/src/Select.h
@@ -1,7 +1,7 @@
 #ifndef Select_h
 #define Select_h
 
-int waitforfds(int socket, char* buffer, sharedmem * shm); // nur ein Vorschlag der Parametrisierung, bitte feststellen, was tatsaechlich benoetigt wird
-int doMove(int sock, char* buffer);
+int waitforfds(int socket, char* buffer, sharedmem * shm, int fd[]); // nur ein Vorschlag der Parametrisierung, bitte feststellen, was tatsaechlich benoetigt wird
+int doMove(int sock, char* buffer, int fd[]);
 
 #endif
