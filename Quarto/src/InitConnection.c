d2fb2b051899fa0779ff8fc978fb4398672f4320
Edd <edina90@hotmail.com>
2014-01-20 21:49:53 +0100

    Update InitConnection.c

--- Quarto/src/InitConnection.c
+++ Quarto/src/InitConnection.c
@@ -10,47 +10,46 @@
 #include "PerformConnection.h"
 
 /**
-* Herstellung der Verbindung zum Server
-*
-* @param Pointer auf SHM, Struktur und pipe
-* @return 0 falls Verbindung hergestellt und performConnection ausfuehrbar
-*/
+ * Herstellung der Verbindung zum Server
+ *
+ * @param Pointer auf SHM, Struktur und pipe
+ * @return 0 falls Verbindung hergestellt und performConnection ausfuehrbar
+ */
 int initConnection(sharedmem * shm, config_struct* conf, int fd[]) {
+	shm->sock = socket(AF_INET, SOCK_STREAM, 0);
+	writelog(logdatei, AT);
+	struct sockaddr_in host;
+	struct hostent* ip;
 
-        shm->sock = socket(AF_INET, SOCK_STREAM, 0);
-        writelog(logdatei, AT);
-        struct sockaddr_in host;
-        struct hostent* ip;
+	/* Uebersetzt Hostname in IP Adresse */
+	ip = (gethostbyname(conf->hostname));
+	if (ip == NULL ) {
+		perror("\nFehler beim Anfordern der IP");
+		writelog(logdatei, AT);
+	}
 
-        /* Uebersetzt Hostname in IP Adresse */
-        ip = (gethostbyname(conf->hostname));
-        if (ip== NULL) {
-             perror("\nFehler beim Anfordern der IP");
-             writelog(logdatei, AT);
-        }
+	memcpy(&(host.sin_addr), ip->h_addr,ip ->h_length);
+	host.sin_family = AF_INET;
+	host.sin_port = htons(conf->portnumber);
 
-        memcpy(&(host.sin_addr), ip->h_addr,ip ->h_length);
-        host.sin_family = AF_INET;
-        host.sin_port = htons(conf->portnumber);
+	if (connect(shm->sock, (struct sockaddr*) &host, sizeof(host)) == 0) {
+		printf("\nVerbindung mit %s hergestellt!\n", conf->hostname);
+		writelog(logdatei, AT);
+	} else {
+		perror("\n Fehler beim Verbindungsaufbau");
+		return EXIT_FAILURE;
+	}
 
-        if (connect(shm->sock, (struct sockaddr*) &host, sizeof(host)) == 0) {
-                printf("\nVerbindung mit %s hergestellt!\n", conf->hostname);
-                writelog(logdatei, AT);
-        } else {
-                perror("\n Fehler beim Verbindungsaufbau");
-                return EXIT_FAILURE;
-        }
+	/* PID in SHM schreiben */
+	shm->pidDad = getppid();
+	shm->pidKid = getpid();
 
-        /* PID in SHM schreiben */
-        shm->pidDad = getppid();
-        shm->pidKid = getpid();
+	/*Fuehre Prolog Protokoll aus */
+	if (performConnection(shm->sock, shm, conf, fd) != 0) {
+		close(shm->sock);
+		return EXIT_FAILURE;
+	}
 
-        /*Fuehre Prolog Protokoll aus */
-        if (performConnection(shm->sock, shm, conf, fd) != 0) {
-                close(shm->sock);
-                return EXIT_FAILURE;
-        }
-
-        close(shm->sock);
-        return EXIT_SUCCESS;
+	close(shm->sock);
+	return EXIT_SUCCESS;
 }
