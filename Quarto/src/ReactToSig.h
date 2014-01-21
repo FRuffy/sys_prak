2f0eb49c9fa90085b79518c00ae0c8e94962ade4
flo-lo <flo.lochner@campus.lmu.de>
2014-01-21 15:49:12 +0100

    Update ReactToSig.h
    SHM wird jetzt auch bei CTRL + C detached und zerstört.

--- Quarto/src/ReactToSig.h
+++ Quarto/src/ReactToSig.h
@@ -1,6 +1,6 @@
 #ifndef ReactToSig_h
 #define ReactToSig_h
 
-int reactToSig(sharedmem * shm, int signal, config_struct *conf, int fd[]);
+int reactToSig(sharedmem * shm, int signal, config_struct *conf, int fd[], int shmID);
 
 #endif
