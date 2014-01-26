6a05729443ed25b78f488fc49a384c88ffeef22b
Muahaas <fabian.ruffy@gmail.com>
2014-01-26 18:52:46 +0100

    Update QuartoThinker.h

--- Quarto/src/QuartoThinker.h
+++ Quarto/src/QuartoThinker.h
@@ -3,7 +3,6 @@
 
 int think(sharedmem *shm);
 char* formatMove(int move);
-char* formatMove1(int move);
 int testStone(sharedmem * shm, int stone);
 void chooseStone(sharedmem * shm);
 int calculateMove(sharedmem *shm, char* stones, int silent);
